8bfc454997d32d5bea0d1b68bb8528fa1738aff6
MayaraQuadros <g00472144@atu.ie>
2024-10-25 11:50:59 +0100

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -2,6 +2,6 @@
 public class Welcome{
  public static void main(String[] args){
  System.out.println("Welcome to Github lab 2!");
- System.out.println("lpA making changes again and again."); 
+ System.out.println("lpA---- making changes again and again."); 
  }
  }
